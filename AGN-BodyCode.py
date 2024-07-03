891c11ad131130b7bc729f4037013416a7814fe3
kbryant25 <karinanbryant@gmail.com>
2024-07-03 11:11:28 -0400

    Update AGN-BodyCode.py
    Forgot plot titles

--- AGN-BodyCode.py
+++ AGN-BodyCode.py
@@ -10,27 +10,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-#%%
+
 # Set Parameters --------------------------------------------------------------
 
-BHM = 10**6    # Black hole mass (Solar masses)
+BHM = 10**6    # Black hole mass
 KV = 15        # Kick velocity in 2piAU/year = 29.7858905 km / s
-dt = 5         # Timestep in years
+dt = 5         # Timestep
 RS = 42        # Set seed for repeatability
 Nimg = 200     # Number of frames (Note: when decreasing number make sure to delete images from animation file)
 KVkms = KV * 29.7858905
 print('Recoil velocity in km/s: ' + str(KVkms))
 
-plt.ioff() # Tells Spyder to only save the plots and not display them
+plt.ioff() #Tells spyder to only save the plots and not display them
 
 
 # Simulation ------------------------------------------------------------------
 
-"""
-Set up the particles in the simulation. Particles are added by default in
-Jacobi coordinates (center-of-mass frame). Default Rebound units are in 'year',
-'AU', 'SolarMass' so G = 1
-"""
+"""Set up the particles in the simulation. Particles are added by default in
+Jacobi coordinates (center-of-mass frame)"""
 
 sim = rebound.Simulation()    # Initialize the simulation
 sim.add(m = BHM)              # Add the central particle
@@ -39,7 +36,7 @@ np.random.seed(RS)            # Random seed for test cases
 
 # Set up the test particles ---------------------------------------------------
 
-N_testparticle = 1000         # Number of test particles
+N_testparticle = 1000         #Number of test particles
 a_initial = np.linspace(4, 8, N_testparticle)
 for a in a_initial:
     sim.add(a=a,f=np.random.rand()*2.*np.pi) # Mass is set to 0 by default, random true anomaly
@@ -47,13 +44,11 @@ for a in a_initial:
 
 # Save sim to binary file------------------------------------------------------
 
-# sim.save_to_file('path/AGNBaseCode.bin')
-"""
-Use to save to binary file, includes ALL information about the particles
+# sim.save_to_file('AGNBaseCode.bin')
+""" Use to save to binary file, includes ALL information about the particles
 (mass, position, velocity, etc), as well as the current simulation settings
 such as time, integrator choice.
-Only needs to be done once
-"""
+Only needs to be done once """
 
 
 # Integration -----------------------------------------------------------------
@@ -86,11 +81,12 @@ for i in range(Nimg):
         plt.scatter(0,0, alpha = 0)
         plt.axis('off')
         plt.tick_params(left = False, right = False , labelleft = False , labelbottom = False, bottom = False)
+        ax4.set_title("SMBH Recoil [Distance in AU]", fontsize = 10)
         sim.step()
     else:
         #Add velocity kick
         sim.move_to_hel()            # Move to the heliocentric reference frame
-        sim.particles[0].vz = KV     # Changing the z velocity of the central particle to KV (in code units)
+        sim.particles[0].vz = KV     # Changing the z velocity of the central particle to __ (in code units)
         sim.move_to_hel()            # Move into the new central particle-frame
         sim.dt = dt
         sim.step()
@@ -119,13 +115,14 @@ for i in range(Nimg):
         plt.scatter(0,0, alpha = 0)
         plt.axis('off')
         plt.tick_params(left = False, right = False , labelleft = False , labelbottom = False, bottom = False)
-    plt.savefig("path/image_%s.jpg" % i)
-    sim.save_to_file("path/archive.bin") # Saves all sim data for further analysis
+        ax4.set_title("SMBH Recoil [Distance in AU]", fontsize = 10)
+    plt.savefig("C:/Users/karin/Desktop/Spyder/Capstone/REBOUND/AnimationFolder/image_%s.jpg" % i)
+    sim.save_to_file("C:/Users/karin/Desktop/Spyder/Capstone/REBOUND/archive.bin")
     #Saving each plot to a file with it's own name to be created into a gif https://ezgif.com/maker
     plt.close()
 
 
-# Print parameters for easy copy-pasting --------------------------------------
+# Print parameters for easy copy-pasting
 
 # print('BH Mass: ' + str(BHM ))
 # print('Number of test Particles: ' + str(N_testparticle))
